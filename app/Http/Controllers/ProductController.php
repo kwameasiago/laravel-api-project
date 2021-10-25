b04c0a71c49ab9e5771e1dd486ce502a572d2664
kwameasiago <kwameasiago@gmail.com>
2021-10-25 17:52:37 +0300

    add logger

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+
 use App\Models\Products;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
@@ -15,6 +17,7 @@ class ProductController extends Controller
     public function index()
     {
         //
+        
         return Products::all();
     }
 
@@ -32,7 +35,22 @@ class ProductController extends Controller
             "price" => "required",
             "slug" => "required"
         ]);
-        return Products::create($request->all());
+
+        Log::info('Skipping ********************'.PHP_EOL);
+        Log::info('This is some useful information.'.PHP_EOL.PHP_EOL, 
+            [
+                $request->all(),
+                $request->url(),
+                $request->isMethod('POST'),
+                $request->bearerToken(),
+                $ipAddress = $request->ip(),
+                $request->query(),
+                $request->getContent()
+            ]
+        );
+        Log::info(PHP_EOL.PHP_EOL.'Skipping ********************'.PHP_EOL.PHP_EOL);
+        // return Products::create($request->all());
+        return $request->all();
     }
 
     /**
